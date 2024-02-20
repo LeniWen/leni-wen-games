cd67fcdcba71e56eecef6be5aff1d8a1c5526c6c
lenianwain <its9527@outlook.com>
2024-02-20 13:30:03 +0800

    feat: main menu page

--- app/routes/hangman.tsx
+++ app/routes/hangman.tsx
@@ -10,10 +10,10 @@ export const meta: MetaFunction = () => {
 
 export default function Hangman() {
   return (
-    <div className="relative grid h-screen grid-rows-[2fr,65%,1fr] overflow-auto bg-hangman px-6">
+    <div className="relative grid h-screen grid-rows-[2fr,65%,1fr] overflow-auto bg-hangman px-6 tablet:px-20">
       <div aria-hidden />
       <div
-        className="relative grid grid-rows-[25%,1fr,15%] rounded-[3rem]"
+        className="relative grid grid-rows-[25%,1fr,15%] rounded-[3rem] tablet:mx-auto tablet:w-[31rem]"
         style={{
           backgroundImage: 'linear-gradient(180deg, #344ABA 0%, rgba(0, 20, 121, 0.83) 100%)',
           boxShadow: 'inset 0px -8px 0px 4px #140E66, inset 0px 6px 0px 8px #2463FF',
@@ -21,10 +21,10 @@ export default function Hangman() {
       >
         <img loading="lazy" decoding="async" src="/images/hangman/logo.svg" alt="logo" className="absolute left-1/2 top-0 max-w-[90%] -translate-x-1/2 -translate-y-1/2" />
         <div aria-hidden />
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col justify-between px-8">
           <Link
             to="category"
-            className="mx-auto w-fit rounded-full p-14"
+            className="mx-auto rounded-full p-14"
             style={{
               backgroundImage: 'linear-gradient(180deg, #FE71FE 16.42%, #7199FF 100%)',
               boxShadow: 'inset 0px -4px 0px 5px #243041, inset 0px -12px 0px 11px #9D2DF5',
@@ -37,7 +37,7 @@ export default function Hangman() {
             style={{
               boxShadow: 'inset 0px -2px 0px 3px #140E66, inset 0px 1px 0px 6px #3C74FF',
             }}
-            className="mx-auto w-[90%] rounded-[2.5rem] bg-[#2463ff] py-3 text-center text-3xl text-white"
+            className="mx-auto w-full max-w-64 rounded-[2.5rem] bg-[#2463ff] py-3 text-center text-3xl text-white"
             type="submit"
           >
             HOW TO PLAY

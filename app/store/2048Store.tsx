106acaa5f932efdc0110d50827f062d37bc3bc37
lewaNi <its9527@outlook.com>
2024-03-05 00:47:53 +0800

    feat: refactor 2048 move logic

---
+++ app/store/2048Store.tsx
@@ -0,0 +1,256 @@
+import { proxy } from 'valtio'
+
+const ROWS = 4
+const COLS = 4
+const START_TILES = 2
+const WON_SCORE = 2048
+/**
+ * Each tile's value is 2 or 4, the possibility of 2 is 90%
+ */
+const TWO_POSSIBILITY = 0.9
+
+interface Position {
+  x: number
+  y: number
+}
+interface Tile {
+  x: Position['x']
+  y: Position['y']
+  value: number
+  key: string
+  mergedFrom?: [Tile, Tile]
+  prevPosition?: Position
+}
+type Cell = Tile | null
+interface Store {
+  cells: Cell[]
+  score: number
+  bestScore: number
+  over: boolean
+  won: boolean
+}
+enum Direction {
+  Up,
+  Right,
+  Down,
+  Left,
+}
+interface MoveDirection {
+  x: -1 | 0 | 1
+  y: -1 | 0 | 1
+}
+
+const game2048Store = proxy<Store>({
+  cells: Array(ROWS * COLS).fill(null),
+  score: 0,
+  bestScore: 0,
+  over: false,
+  won: false,
+})
+
+function availableCells(): Position[] {
+  const positions = []
+
+  for (let x = 0; x < ROWS; x++) {
+    for (let y = 0; y < COLS; y++) {
+      if (!getCell({ x, y }))
+        positions.push({ x, y })
+    }
+  }
+
+  return positions
+}
+
+function createTile(position: Position, value: Tile['value']): Tile {
+  return {
+    ...position,
+    value,
+    key: crypto.randomUUID(),
+  }
+}
+
+function insertTile(tile: Tile) {
+  game2048Store.cells[tile.x + tile.y * COLS] = tile
+}
+
+function clearTile(tile: Tile) {
+  game2048Store.cells[tile.x + tile.y * COLS] = null
+}
+
+function addRandomTile() {
+  const cells = availableCells()
+
+  if (cells.length) {
+    const value = Math.random() < TWO_POSSIBILITY ? 2 : 4
+    const index = Math.floor(Math.random() * cells.length)
+    const tile = createTile(cells[index], value)
+
+    insertTile(tile)
+  }
+  else {
+    throw new Error('No room to add a tile')
+  }
+}
+
+/**
+ * The direction of tile movement
+ *
+ * @example
+ *
+ * Initial grid cells:
+ * 0 2 0 2
+ * After pressing the Arrow Right key:
+ * 0 0 0 4
+ * All tiles move to the right, and identical tiles merge together
+ */
+function getTileMoveDirection(direction: Direction): MoveDirection {
+  const map: Record<Direction, MoveDirection> = {
+    [Direction.Up]: { x: 0, y: -1 },
+    [Direction.Right]: { x: 1, y: 0 },
+    [Direction.Down]: { x: 0, y: 1 },
+    [Direction.Left]: { x: -1, y: 0 },
+  }
+
+  return map[direction]
+}
+
+/**
+ * Creates the order for traversing grid cells based on the direction of tile movement
+ * This function determines the sequence in which grid cells should be visited when tiles
+ * are moving in a particular direction
+ *
+ * @example
+ *
+ * Initial grid cells:
+ * 0 2 0 2
+ * 4 2 4 0
+ * After pressing the Arrow Right key:
+ * Traversal order changes to ensure tiles move and merge correctly:
+ * 0 0 0 4
+ * 0 4 2 4
+ */
+function createTraversalOrder(moveDirection: MoveDirection) {
+  const traverse: { x: number[], y: number[] } = { x: [], y: [] }
+
+  for (let x = 0; x < ROWS; x++)
+    traverse.x.push(x)
+  for (let y = 0; y < COLS; y++)
+    traverse.y.push(y)
+  if (moveDirection.x === 1)
+    traverse.x.reverse()
+  if (moveDirection.y === 1)
+    traverse.y.reverse()
+
+  return traverse
+}
+
+function withinBounds(p: Position): boolean {
+  return p.x >= 0 && p.x < ROWS && p.y >= 0 && p.y < COLS
+}
+
+function cellAvailable(p: Position): boolean {
+  return !!getCell(p)
+}
+
+function prepareTiles() {
+  game2048Store.cells.forEach((cell) => {
+    if (!cell)
+      return
+    cell.prevPosition = { x: cell.x, y: cell.y }
+    delete cell.mergedFrom
+  })
+}
+
+function moveTile(tile: Tile, p: Position) {
+  game2048Store.cells[tile.x + tile.y * COLS] = null
+  game2048Store.cells[p.x + p.y * COLS] = tile
+  tile.x = p.x
+  tile.y = p.y
+}
+
+function getCell(p: Position) {
+  return game2048Store.cells[p.x + p.y * COLS]
+}
+
+function canMove(): boolean {
+  if (availableCells().length)
+    return true
+  for (let x = 0; x < ROWS; x++) {
+    for (let y = 0; y < COLS; y++) {
+      const tile = getCell({ x, y })
+
+      if (!tile)
+        continue
+
+      for (let direction = 0; direction < 4; direction++) {
+        const moveDirection = getTileMoveDirection(direction)
+        const anotherTile = getCell({ x: x + moveDirection.x, y: moveDirection.y })
+
+        if (anotherTile && anotherTile.value === tile.value)
+          return true // These two tiles can be merged
+      }
+    }
+  }
+
+  return false
+}
+
+export function move(direction: Direction) {
+  if (game2048Store.over || game2048Store.won)
+    return
+
+  prepareTiles()
+
+  const moveDirection = getTileMoveDirection(direction)
+  const traverse = createTraversalOrder(moveDirection)
+  let moved = false
+
+  traverse.x.forEach((x) => {
+    traverse.y.forEach((y) => {
+      // The farthest position to which the tile can move without merging
+      let farthest: Position = { x, y }
+      const tile = getCell(farthest)
+
+      if (!tile)
+        return
+
+      // The next position where a merge is possible
+      let next: Position | null = { x: x + moveDirection.x, y: y + moveDirection.y }
+
+      while (withinBounds(next) && cellAvailable(next)) {
+        farthest = next
+        next = { x: x + moveDirection.x, y: y + moveDirection.y }
+      }
+
+      const nextTile = withinBounds(next) ? getCell(next) : null
+
+      if (nextTile && !nextTile.mergedFrom && nextTile.value === tile.value) {
+        const merged = createTile(next, tile.value * 2)
+
+        merged.mergedFrom = [tile, nextTile]
+
+        tile.x = nextTile.x
+        tile.y = nextTile.y
+
+        insertTile(merged)
+        clearTile(tile)
+
+        game2048Store.score += merged.value
+
+        if (game2048Store.score === WON_SCORE)
+          game2048Store.won = true
+      }
+      else {
+        moveTile(tile, farthest)
+      }
+      if (!(farthest.x === tile.x && farthest.y === tile.y)) // not the same position
+        moved = true
+    })
+  })
+  if (moved) {
+    addRandomTile()
+
+    if (!canMove())
+      game2048Store.over = true
+  }
+}
